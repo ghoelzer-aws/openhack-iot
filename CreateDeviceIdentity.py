f4f1689e2cb4eb814a6e6f312d282246a386e463
Greg Hoelzer <grhoelze@microsoft.com>
2018-01-31 10:57:53 -0500

    updateds connect string

--- CreateDeviceIdentity.py
+++ CreateDeviceIdentity.py
@@ -3,7 +3,7 @@ import iothub_service_client
 from iothub_service_client import IoTHubRegistryManager, IoTHubRegistryManagerAuthMethod
 from iothub_service_client import IoTHubDeviceStatus, IoTHubError
 
-CONNECTION_STRING = "HostName=gregsiothub01.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ETpP5xo46gnsKYSWyG0Jkc117DU1hpT8D210CHTkqpY="
+CONNECTION_STRING = "HostName=gregsiothub01.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GHT/TWPao3YuEHLB18L9+JckwW4neFt52mPGVo8TzCw="
 DEVICE_ID = "GregsFirstPythonDevice"
 
 def print_device_info(title, iothub_device):
